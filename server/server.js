be94fcfe454468d189f024f0c4372487094dbdc9
Mladen <mladja92@yahoo.com>
2017-02-02 17:14:31 +0100

    Added server.js and mongoose

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+const Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+const newUser = new User({
+  email: 'mladja92@yahoo.com'
+});
+
+newUser.save().then((doc) => {
+  console.log('Saved user', doc);
+}, (e) => {
+  console.log('Unable to save user', e);
+});
+
+
+// const newTodo = new Todo({
+//   text: 'Something to do'
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo', e);
+// });

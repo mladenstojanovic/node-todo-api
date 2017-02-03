d291518cab46ea169b2d94007ad3ad7ffa46a1f6
Mladen <mladja92@yahoo.com>
2017-02-03 23:22:05 +0100

    Added Delete /todo/:id

--- server/server.js
+++ server/server.js
@@ -46,6 +46,25 @@ app.get('/todos/:id', (req, res) => {
   })
 });
 
+app.delete('/todos/:id', (req,res) => {
+  //get the id
+  const id = req.params.id;
+  // validate the ide -> not valid? return 404
+  if (!ObjectID.isValid(id)) {
+    res.status(400).send();
+  }
+  //remove todo by id
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send(todo);
+
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
